6956aec380294b21960b55d0c03a57a7af3fea82
Japnitsingh123 <japnitsingh2005@gmail.com>
2025-07-15 20:21:18 +0530

    updated

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ with st.sidebar:
 image = None
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.sidebar.image(image, caption="Uploaded Image", use_container_width=True)
+    st.sidebar.image(image, caption="Uploaded Image", use_column_width=True)
 
 # Prompt for Gemini
 input_prompt = """
